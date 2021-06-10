6512ff303318e2ef98bc3a22af0378a2ead23ede
Ashish <ashish.gurutechnolabs@gmail.com>
2021-06-10 14:44:24 +0530

    init commit

---
+++ Gethingd/Gethingd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gethingd
+//
+//  Created by Ashish on 10/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
